396e3ee0aa22a5ed6545c450a617dd8ec8490399
jcpapernik <jpawesome101@gmail.com>
2026-07-26 17:51:32 -0400

    Updates

--- _next/static/2Qge9TbJd7Eev586vSdtg/_buildManifest.js
+++ _next/static/2Qge9TbJd7Eev586vSdtg/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/test-repo//_next/:path+",
+        "destination": "/test-repo/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
